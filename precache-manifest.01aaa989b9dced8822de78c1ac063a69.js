aa6169c28f2227f6bfd21e1e80e6e7b68cbff49e
unknown <tanishdragonemperor>
2021-12-03 23:16:12 +0530

    Updates

---
+++ precache-manifest.01aaa989b9dced8822de78c1ac063a69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6bb5fd8c5529c5efe76595129da6574",
+    "url": "/reactpro2/index.html"
+  },
+  {
+    "revision": "0c429774b56c68f629f8",
+    "url": "/reactpro2/static/css/main.ca1f696f.chunk.css"
+  },
+  {
+    "revision": "1407c86a95fc80bc2054",
+    "url": "/reactpro2/static/js/2.8ed737be.chunk.js"
+  },
+  {
+    "revision": "0c429774b56c68f629f8",
+    "url": "/reactpro2/static/js/main.cead57c4.chunk.js"
+  },
+  {
+    "revision": "5654640e4c7af19dc3a7",
+    "url": "/reactpro2/static/js/runtime~main.56523aa1.js"
+  }
+]);
